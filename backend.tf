e8bfccfe34c3c8ef9e31701c553458129e91e3b6
wahaj akmal <44682195+wahaj123@users.noreply.github.com>
2020-10-16 03:22:39 +0500

    first commit

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "wahaj-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "wahaj-lock"
+  }
+}
